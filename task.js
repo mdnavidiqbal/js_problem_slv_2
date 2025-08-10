87862c6c0aefc4d97a439eb8f6ceb2b479e3aaad
mdnavidiqbal <navidiqbal2000@gmail.com>
2025-08-11 01:42:14 +0600

    write also some code

--- task.js
+++ task.js
@@ -4,3 +4,19 @@
 
 const heights2 = Math.min(167, 190, 120, 165, 137);
 console.log(Math.min(heights2));
+
+// Task -2:
+// Find the friend with the smallest name.
+// const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+
+const heights3 = ['rahim', 'robin', 'rafi', 'ron', 'rashed', 'ak'];
+let smallestName = heights3[0];
+for (const name of heights3) {
+    if (name.length < smallestName.length) {
+        smallestName = name;
+    }
+}
+console.log("The smallest name is : ", smallestName);
+
+
+
