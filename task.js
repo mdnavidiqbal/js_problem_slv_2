1d9d1bf523e303a779266a518347161e8fc865df
mdnavidiqbal <navidiqbal2000@gmail.com>
2025-08-11 01:15:50 +0600

    write a short code

--- task.js
+++ task.js
@@ -1,1 +1,6 @@
-console.log("Hello!World");
+// Task -1:
+// Find the lowest number in the array below.
+// const heights2 = [167, 190, 120, 165, 137];
+
+const heights2 = Math.min(167, 190, 120, 165, 137);
+console.log(Math.min(heights2));
