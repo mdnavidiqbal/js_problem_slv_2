2a873fab6a434803fcd2400158a1162c88086091
mdnavidiqbal <navidiqbal2000@gmail.com>
2025-08-11 12:03:34 +0600

    Add a normal function for average

--- task.js
+++ task.js
@@ -34,3 +34,48 @@ function calculateElectronicsBudget(item1, item2, item3) {
 let price = calculateElectronicsBudget(35000, 15000, 20000);
 console.log("The total price is : ", price);
 
+// Task-4:
+// You are given an array of phone objects, each containing information about the model, brand, and price. Your task is to write a JavaScript function named findAveragePhonePrice that takes this array as input and returns the average price of phone.
+
+// Input
+
+
+//     const phones = [
+//         { model: "PhoneA", brand: "Iphone", price: 95000 },
+//         { model: "PhoneB", brand: "Samsung", price: 40000 },
+//         { model: "PhoneC", brand: "Oppo", price: 26000 },
+//         { model: "PhoneD", brand: "Nokia", price: 35000 },
+//         { model: "PhoneE", brand: "Iphone", price: 105000 },
+//         { model: "PhoneF", brand: "HTC", price: 48000 },
+//     ];
+
+function countAverage(arr){
+    
+let priceArrary = [];
+for( let phone of phones){
+    priceArrary.push(phone.price);
+}
+console.log("The phone prices array : ",priceArrary);
+let lenArray = priceArrary.length;
+console.log("The length of array is : ",lenArray);
+sum = 0;
+for(num of priceArrary){
+    sum += num;
+}
+console.log("The sum of phone prices is : ",sum);
+const average = sum / lenArray;
+return average;
+}
+
+const phones = [
+    { model: "PhoneA", brand: "Iphone", price: 95000 },
+    { model: "PhoneB", brand: "Samsung", price: 40000 },
+    { model: "PhoneC", brand: "Oppo", price: 26000 },
+    { model: "PhoneD", brand: "Nokia", price: 35000 },
+    { model: "PhoneE", brand: "Iphone", price: 105000 },
+    { model: "PhoneF", brand: "HTC", price: 48000 },
+];
+
+const average = countAverage(phones);
+console.log("The average of mobile prices is : ",average.toFixed(2)); // toFixed() we use it show the number after (.)
+
